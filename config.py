f48cbdbfa7b6a2d69320b218f2917cfee57fb998
Jodmarmik <155324790+Jodmarmik@users.noreply.github.com>
2024-12-26 18:24:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,13 +4,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "24833955"
+API_ID = "12914467"
 # -------------------------------------------------------------
-API_HASH = "176360aef3e85805ea17d304adf63f18"
+API_HASH = "622ce5593a8652658da8cf5b4a772086"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("7872756754:AAFR7DH3U8tnUPoBPHpnwi3q_8IXGGMh1AQ", None)
-MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = int(getenv("OWNER_ID", "6474552945"))
-SUPPORT_GRP = "+nJZBaGgAUz84NDQ0"
+MONGO_URL = getenv("mongodb+srv://ophudoston:ophudoston@ophudoston.31rt2.mongodb.net/?retryWrites=true&w=majority&appName=ophudoston", None)
+OWNER_ID = int(getenv("OWNER_ID", "7659960433"))
+SUPPORT_GRP = "https://t.me/huntermafiya"
 UPDATE_CHNL = "Dpz_V"
 OWNER_USERNAME = "ll_VIBER_ll"
